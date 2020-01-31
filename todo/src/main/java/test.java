9449af7de0f132e077f37650e9cf899104eaca65
martin <1156509299@qq.com>
2020-01-31 12:03:31 +0800

    gradle的入门学习
    Signed-off-by: martin <1156509299@qq.com>

---
+++ todo/src/main/java/test.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class test {
+
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(9);
+    }
+
+    void fun1(int i) {
+        System.out.println(i);
+    }
+}
